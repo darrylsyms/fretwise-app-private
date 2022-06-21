8ac0d4c05166b27aa128e257669dada6503c696f
darrylsyms <hello@darrylsyms.com>
2022-06-21 12:40:59 +1000

    del

--- components/Global/CoreImageBlock.js
+++ components/Global/CoreImageBlock.js
@@ -1,47 +1,17 @@
 import React from "react";
 import {
-    Text,
-    View,
-    StyleSheet,
-    Dimensions,
-    Animated,
     Image,
 } from 'react-native';
-import { PinchGestureHandler, State } from 'react-native-gesture-handler';
+import Pinchable from 'react-native-pinchable';
 
 const ImageComponent = (props) => {
 
     const { block } = props;
 
-    const scale = new Animated.Value(1);
-
-    const onZoomEventFunction = Animated.event(
-        [{
-            nativeEvent: { scale: scale },
-        }],
-        {
-            useNativeDriver: true,
-        }
-    );
-
-    const onZoomStateChangeFunction = (event) => {
-        if (event.nativeEvent.oldState == State.ACTIVE) {
-            Animated.spring(scale, {
-                toValue: 1,
-                useNativeDriver: true,
-            }).start();
-        }
-    };
-
     return (
-        <View style={{ flex: 1 }}>
-            <PinchGestureHandler
-                onGestureEvent={onZoomEventFunction}
-                onHandlerStateChange={onZoomStateChangeFunction}
-            >
-                <Animated.Image source={{ uri: block?.content }} style={{ paddingHorizontal: 17, borderRadius: 12, aspectRatio: block.style.width / block.style.height, transform: [{ scale: scale }] }} resizeMode="contain" />
-            </PinchGestureHandler>
-        </View>
+            <Pinchable style={{ flex: 1 }}>
+                <Image source={{ uri: block?.content }} style={{ paddingHorizontal: 17, borderRadius: 12, aspectRatio: block.style.width / block.style.height }} resizeMode="contain" />
+            </Pinchable>
     )
 
 }
