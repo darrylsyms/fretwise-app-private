5b58ff89a9d1add04768ffc516cb134129acd65e
Darryl <82924984+darrylsyms@users.noreply.github.com>
2023-04-30 00:06:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import React from "react";
 
 
+
+import BlogItem from "./src/components/ListItems/BlogItem";
+
+
 export const applyCustomCode = externalCodeSetup => {
 
 
+
+	/*-----------------------------------------------------------------------------------*/
+	/* BLOG / DAILY CHALLENGES */
+	/*-----------------------------------------------------------------------------------*/
+
+	externalCodeSetup.blogApi.setBlogItemComponent(props => <BlogItem {...props} />);
+	externalCodeSetup.blogApi.hideSearch();
+	externalCodeSetup.blogSingleApi.setTransformBlogHeaderButtons(() => {return null}); // hide comments icon on blog
 
 };
