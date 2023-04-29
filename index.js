d77ed5fe81138b140821314c983e4aaf78f87258
Darryl <82924984+darrylsyms@users.noreply.github.com>
2023-04-29 22:52:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 import React from "react";
-import { Platform, View } from "react-native";
-import { DEVICE_WIDTH, GUTTER } from "@src/styles/global";
-import {
-	SAFE_AREA_BOTTOM,
-	IOS_HOME_INDICATOR,
-	LESSON_ACTION_BUTTON_HEIGHT,
-	LESSON_ACTION_BUTTON_TEXT_MARGIN,
-	LESSON_ACTION_BUTTON_ICON_MARGIN
-} from "./src/styles/global";
-import { isTabletOrIPad } from "@src/utils";
 
-// * Index Screen Defaults * //
-import FilterBarComponents from "./src/components/Global/ReplaceFilterBarComponent";
+
 // * User Profile * //
 import AfterProfileDetails from "./src/components/Screens/ProfileScreen/AfterProfileDetails";
 import ProfileHeaderButton from "./src/components/Screens/ProfileScreen/ProfileHeaderRightButton";
@@ -103,66 +92,5 @@ export const applyCustomCode = externalCodeSetup => {
 	externalCodeSetup.blogApi.setBlogItemComponent(props => <BlogItem {...props} />);
 	externalCodeSetup.blogApi.hideSearch();
 	externalCodeSetup.blogSingleApi.setTransformBlogHeaderButtons(() => {return null}); // hide comments icon on blog
-
-
-
-	/*-----------------------------------------------------------------------------------*/
-	/* COSMETIC STYLES */
-	/*-----------------------------------------------------------------------------------*/
-
-	externalCodeSetup.cssApi.addGlobalStyle("xProfileItemTitle", { fontSize: 18, paddingBottom: 10 });
-	externalCodeSetup.cssApi.addGlobalStyle("xProfileItemValue", { fontSize: 15 });
-	externalCodeSetup.cssApi.addGlobalStyle("xProfileItemTextArea", { fontSize: 14 });
-	externalCodeSetup.cssApi.addGlobalStyle("localVideoContainer", { borderRadius: 14 }, true);
-	externalCodeSetup.cssApi.addGlobalStyle("localVideoPlayer", { borderRadius: 14 }, true);
-	externalCodeSetup.cssApi.addGlobalStyle("offlineVideoContainer", { borderRadius: 14 }, true);
-	// Lesson Bottom Action Button
-	externalCodeSetup.cssApi.addGlobalStyle("lessonActionButtonContainer", { paddingHorizontal: 0, paddingVertical: 0, marginBottom: -SAFE_AREA_BOTTOM });
-	externalCodeSetup.cssApi.addGlobalStyle("completeLessonButtonW", { borderRadius: 0, height: LESSON_ACTION_BUTTON_HEIGHT });
-	externalCodeSetup.cssApi.addGlobalStyle("completeLessonButton", { marginLeft: 0, marginBottom: Platform.OS === 'ios' ? 15 : 0 });
-	externalCodeSetup.cssApi.addGlobalStyle("completeButton", { marginBottom: LESSON_ACTION_BUTTON_TEXT_MARGIN }); // lesson complete text
-	externalCodeSetup.cssApi.addGlobalStyle("lessonActionCompleteIcon", { marginBottom: LESSON_ACTION_BUTTON_ICON_MARGIN, width: 30, height: 30 });
-	externalCodeSetup.cssApi.addGlobalStyle("lessonButtonLoadingIcon", { marginBottom: IOS_HOME_INDICATOR, left: -5 });
-	// Topic Bottom Action Button
-	externalCodeSetup.cssApi.addGlobalStyle("learnTopicActionButtonContainer", { paddingHorizontal: 0, paddingVertical: 0, marginBottom: -SAFE_AREA_BOTTOM });
-	externalCodeSetup.cssApi.addGlobalStyle("completeTopicButtonW", { borderRadius: 0, height: LESSON_ACTION_BUTTON_HEIGHT });
-	externalCodeSetup.cssApi.addGlobalStyle("completeTopicButton", { marginBottom: LESSON_ACTION_BUTTON_TEXT_MARGIN, fontWeight: '600', }); // topic complete text
-	externalCodeSetup.cssApi.addGlobalStyle("learnTopicActionCompleteIcon", { marginBottom: LESSON_ACTION_BUTTON_ICON_MARGIN, width: 30, height: 30 });
-	externalCodeSetup.cssApi.addGlobalStyle("learnTopicButtonLoadingIcon", { marginBottom: IOS_HOME_INDICATOR, left: -5 });
-	// Lesson Container - For full-width videos
-	//externalCodeSetup.cssApi.addGlobalStyle("lessonSingleScreenBlockContainer", { paddingHorizontal: 0 }, true); // For full width Images
-	//externalCodeSetup.cssApi.addGlobalStyle("learnTopicSingleScreenBlockContainer", { paddingHorizontal: 0 }, true); // For full width Images
-	if (isTabletOrIPad()) externalCodeSetup.cssApi.addGlobalStyle("lessonSingleScreenBlockContainer", { paddingHorizontal: isTabletOrIPad() ? GUTTER : 0 }, true); // For full width Images
-	if (isTabletOrIPad()) externalCodeSetup.cssApi.addGlobalStyle("learnTopicSingleScreenBlockContainer", { paddingHorizontal: GUTTER }, true); // For full width Images
-	externalCodeSetup.cssApi.addGlobalStyle("videoBlockContainer", { paddingHorizontal: 0, paddingVertical: 10, flex: 0 });
-
-
-	/*-----------------------------------------------------------------------------------*/
-	/* FILTER COMPONENTS */
-	/*-----------------------------------------------------------------------------------*/
-
-	// Set default sorting order in Courses lists.
-	externalCodeSetup.coursesHooksApi.setSubFiltersFilter((filters) => {
-		return ['my_progress', 'recent', 'title'];
-	})
-
-	// Set default sorting order in Course Categories lists.
-	externalCodeSetup.coursesHooksApi.setCategoriesSubFiltersFilter((filters) => {
-		return ["date", "title"];
-	})
-
-	// Forum Sub Filters
-	externalCodeSetup.forumsHooksApi.setShowSearch((bbSetting) => false);
-	externalCodeSetup.forumsHooksApi.setSubFiltersFilter((filters) => {
-		return ["popular", "activity"];
-	});
-
-	// Replace Filter Bar components
-	externalCodeSetup.filterScreenApiHooks.setFilterComponent(props => <FilterBarComponents {...props} />)
-
-	// Remove "filter icon"
-	externalCodeSetup.filterScreenApiHooks.setFilterAllButtonHidden(filterType => true);
-
-
 
 };
