3e1028f910f91a2ddc22692a2758a1fcc6ced01b
Darryl <82924984+darrylsyms@users.noreply.github.com>
2023-04-30 20:09:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,49 @@ import OnboardingScreen from "./all/containers/screens/OnboardingScreen";
 /*----------------------*/
 /*      Components      */
 /*----------------------*/
-
+// * LD Content * //
+import LessonTitle from "./all/components/Screens/LessonSingleScreen/LessonTitle";
+import TopicTitle from "./all/components/Screens/LearnTopicSingleScreen/TopicTitle";
+import PrevNextLessons from './all/components/Screens/LessonSingleScreen/PrevNextButtonsLesson';
+import PrevNextTopics from "./all/components/Screens/LearnTopicSingleScreen/PrevNextButtonsTopic";
+import ContentHeadingsBlock from "./all/components/Global/CoreHeadingBlock";
+import ImageComponent from "./all/components/Global/CoreImageBlock";
+import EmbedsComponent from "./all/components/Global/CoreEmbedBlock";
+import LessonActionComponent from "./all/components/Screens/LessonSingleScreen/LessonBottomActionButton";
+import LearnTopicActionComponent from "./all/components/Screens/LearnTopicSingleScreen/TopicBottomActionButton";
+import SpacerComponent from "./all/components/Global/CoreSpacerBlock";
+// * LD Courses * //
+import CourseHeaderItems from "./all/components/Screens/CourseSingleScreen/CourseHeaderDetails";
+import CourseActionButton from "@src/components/Course/CourseActionButton";
+// * Index Screen Defaults * //
+import IndexScreenHeaderHeight from "./all/components/Global/AnimatedHeaderHeight";
+import FilterBarComponents from "./all/components/Global/ReplaceFilterBarComponent";
+import CustomTabBarBottom from "./all/components/Global/TabBarBottom";
+import AnimatedHeaderContents from "./all/components/Global/AnimatedHeaderContents";
+// * User Profile * //
+import AfterProfileDetails from "./all/components/Screens/ProfileScreen/AfterProfileDetails";
+import ProfileHeaderButton from "./all/components/Screens/ProfileScreen/ProfileHeaderRightButton";
+import UserProfileAvatar from "./all/components/Screens/ProfileScreen/ProfileSubscriberBadge";
+// * Forums * //
+import ForumHeaderButtons from './all/components/Screens/ForumsSingleScreen/ForumHeaderRightButtons';
+import HeaderRightComponent from "./all/components/Screens/TopicsScreen/HeaderRight";
+import ReplyItemAvatar from "./all/components/Screens/TopicsSingleScreen/ReplyItemAvatar";
+// * Misc * //
+import GroupDetailsComponent from "./all/components/Screens/GroupSingleScreen/GroupDetailsComponent";
+import MessageText from "./all/components/Screens/MessagesScreen/MessageContents";
+// * Main List Items * //
+import BlogItem from "./all/components/ListItems/BlogItem";
+import TopicItem from "./all/components/ListItems/TopicItem";
+import TopicHeaderAvatar from "./all/components/Screens/TopicsSingleScreen/TopicHeaderItem";
+/*----------------------*/
+/*       Reducers       */
+/*----------------------*/
+import hotTopicsReducer from './all/state/reducers/hotTopics.reducer';
+import coursesReducer from './all/state/reducers/courses.reducer';
+import courseCategoriesReducer from './all/state/reducers/courseCategories.reducer';
+import forumsReducer from './all/state/reducers/forums.reducer';
+import welcomeMessagesReducer from "./all/state/reducers/welcomeMessages.reducer";
+import courseIncludesReducer from "./all/state/reducers/courseIncludes.reducer";
 
 export const applyCustomCode = externalCodeSetup => {
 
