50b5adb795a8e4035029054a999d518a114f2989
Darryl <82924984+darrylsyms@users.noreply.github.com>
2023-04-30 17:01:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,8 @@ import { NavigationActions } from 'react-navigation';
 /*      Components      */
 /*----------------------*/
 // * LD Content * //
-import LessonTitle from "./src/components/Screens/LessonSingleScreen/LessonTitle";
-import TopicTitle from "./src/components/Screens/LearnTopicSingleScreen/TopicTitle";
-import PrevNextLessons from './src/components/Screens/LessonSingleScreen/PrevNextButtonsLesson';
-import PrevNextTopics from "./src/components/Screens/LearnTopicSingleScreen/PrevNextButtonsTopic";
-import LessonActionComponent from "./src/components/Screens/LessonSingleScreen/LessonBottomActionButton";
-import LearnTopicActionComponent from "./src/components/Screens/LearnTopicSingleScreen/TopicBottomActionButton";
-// * LD Courses * //
 import CourseHeaderItems from "./src/components/Screens/CourseSingleScreen/CourseHeaderDetails";
 import CourseActionButton from "@src/components/Course/CourseActionButton";
-
-import FilterBarComponents from "./src/components/Global/ReplaceFilterBarComponent";
-
-// * Forums * //
-import ForumHeaderButtons from './src/components/Screens/ForumsSingleScreen/ForumHeaderRightButtons';
-import ReplyItemAvatar from "./src/components/Screens/TopicsSingleScreen/ReplyItemAvatar";
-
-import TopicItem from "./src/components/ListItems/TopicItem";
-import TopicHeaderAvatar from "./src/components/Screens/TopicsSingleScreen/TopicHeaderItem";
-
 
 export const applyCustomCode = externalCodeSetup => {
 
