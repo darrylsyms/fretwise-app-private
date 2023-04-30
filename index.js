e22ddd7112cd1c4d28480a00e186aaaaee008c89
Darryl <82924984+darrylsyms@users.noreply.github.com>
2023-04-30 23:58:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,7 @@ import { ScreenNames } from "./all/data/ScreensWithoutTabBar";
 /*      Components      */
 /*----------------------*/
 // * LD Content * //
-import LessonTitle from "./all/components/Screens/LessonSingleScreen/LessonTitle";
-import TopicTitle from "./all/components/Screens/LearnTopicSingleScreen/TopicTitle";
-import PrevNextLessons from './all/components/Screens/LessonSingleScreen/PrevNextButtonsLesson';
-import PrevNextTopics from "./all/components/Screens/LearnTopicSingleScreen/PrevNextButtonsTopic";
-import ContentHeadingsBlock from "./all/components/Global/CoreHeadingBlock";
-import ImageComponent from "./all/components/Global/CoreImageBlock";
-import EmbedsComponent from "./all/components/Global/CoreEmbedBlock";
-import LessonActionComponent from "./all/components/Screens/LessonSingleScreen/LessonBottomActionButton";
-import LearnTopicActionComponent from "./all/components/Screens/LearnTopicSingleScreen/TopicBottomActionButton";
-import SpacerComponent from "./all/components/Global/CoreSpacerBlock";
+
 // * LD Courses * //
 import CourseHeaderItems from "./all/components/Screens/CourseSingleScreen/CourseHeaderDetails";
 import CourseActionButton from "@src/components/Course/CourseActionButton";
@@ -33,12 +24,9 @@ import AfterProfileDetails from "./all/components/Screens/ProfileScreen/AfterPro
 import ProfileHeaderButton from "./all/components/Screens/ProfileScreen/ProfileHeaderRightButton";
 import UserProfileAvatar from "./all/components/Screens/ProfileScreen/ProfileSubscriberBadge";
 // * Forums * //
-import ForumHeaderButtons from './all/components/Screens/ForumsSingleScreen/ForumHeaderRightButtons';
-import HeaderRightComponent from "./all/components/Screens/TopicsScreen/HeaderRight";
-import ReplyItemAvatar from "./all/components/Screens/TopicsSingleScreen/ReplyItemAvatar";
+
 // * Misc * //
-import GroupDetailsComponent from "./all/components/Screens/GroupSingleScreen/GroupDetailsComponent";
-import MessageText from "./all/components/Screens/MessagesScreen/MessageContents";
+
 // * Main List Items * //
 
 /*----------------------*/
