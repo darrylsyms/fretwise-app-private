16fba8f1de4bb59416a5e7b4b7675a9da2a9b422
Darryl <82924984+darrylsyms@users.noreply.github.com>
2023-04-30 21:45:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 import React from "react";
-import { Platform, View } from "react-native";
-import {
-	SAFE_AREA_BOTTOM,
-	IOS_HOME_INDICATOR,
-	LESSON_ACTION_BUTTON_HEIGHT,
-	LESSON_ACTION_BUTTON_TEXT_MARGIN,
-	LESSON_ACTION_BUTTON_ICON_MARGIN
-} from "./all/styles/global";
+
 import { NavigationActions } from 'react-navigation';
 import { ScreenNames } from "./all/data/ScreensWithoutTabBar";
 /*----------------------*/
@@ -23,16 +16,7 @@ import OnboardingScreen from "./all/containers/screens/OnboardingScreen";
 /*      Components      */
 /*----------------------*/
 // * LD Content * //
-import LessonTitle from "./all/components/Screens/LessonSingleScreen/LessonTitle";
-import TopicTitle from "./all/components/Screens/LearnTopicSingleScreen/TopicTitle";
-import PrevNextLessons from './all/components/Screens/LessonSingleScreen/PrevNextButtonsLesson';
-import PrevNextTopics from "./all/components/Screens/LearnTopicSingleScreen/PrevNextButtonsTopic";
-import ContentHeadingsBlock from "./all/components/Global/CoreHeadingBlock";
-import ImageComponent from "./all/components/Global/CoreImageBlock";
-import EmbedsComponent from "./all/components/Global/CoreEmbedBlock";
-import LessonActionComponent from "./all/components/Screens/LessonSingleScreen/LessonBottomActionButton";
-import LearnTopicActionComponent from "./all/components/Screens/LearnTopicSingleScreen/TopicBottomActionButton";
-import SpacerComponent from "./all/components/Global/CoreSpacerBlock";
+
 // * LD Courses * //
 import CourseHeaderItems from "./all/components/Screens/CourseSingleScreen/CourseHeaderDetails";
 import CourseActionButton from "@src/components/Course/CourseActionButton";
@@ -50,21 +34,13 @@ import ForumHeaderButtons from './all/components/Screens/ForumsSingleScreen/Foru
 import HeaderRightComponent from "./all/components/Screens/TopicsScreen/HeaderRight";
 import ReplyItemAvatar from "./all/components/Screens/TopicsSingleScreen/ReplyItemAvatar";
 // * Misc * //
-import GroupDetailsComponent from "./all/components/Screens/GroupSingleScreen/GroupDetailsComponent";
-import MessageText from "./all/components/Screens/MessagesScreen/MessageContents";
+
 // * Main List Items * //
-import BlogItem from "./all/components/ListItems/BlogItem";
-import TopicItem from "./all/components/ListItems/TopicItem";
-import TopicHeaderAvatar from "./all/components/Screens/TopicsSingleScreen/TopicHeaderItem";
+
 /*----------------------*/
 /*       Reducers       */
 /*----------------------*/
-import hotTopicsReducer from './all/state/reducers/hotTopics.reducer';
-import coursesReducer from './all/state/reducers/courses.reducer';
-import courseCategoriesReducer from './all/state/reducers/courseCategories.reducer';
-import forumsReducer from './all/state/reducers/forums.reducer';
-import welcomeMessagesReducer from "./all/state/reducers/welcomeMessages.reducer";
-import courseIncludesReducer from "./all/state/reducers/courseIncludes.reducer";
+
 
 export const applyCustomCode = externalCodeSetup => {
 
