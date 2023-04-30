b77f879dbbf101ca117e1113de5794ea03ab0041
Darryl <82924984+darrylsyms@users.noreply.github.com>
2023-04-30 17:34:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { NavigationActions } from 'react-navigation';
 /*      Components      */
 /*----------------------*/
 // * LD Content * //
-import CourseHeaderItems from "./src/components/Screens/CourseSingleScreen/CourseHeaderDetails";
 
+import CourseActionButton from "@src/components/Course/CourseActionButton";
 
 export const applyCustomCode = externalCodeSetup => {
 
