764b5f03bc703b774413a23959e0678a23377026
Darryl <82924984+darrylsyms@users.noreply.github.com>
2023-04-30 23:05:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,23 +7,48 @@ import { ScreenNames } from "./all/data/ScreensWithoutTabBar";
 /*----------------------*/
 import DownloadedCoursesScreen from "@src/containers/DownloadedCoursesScreen";
 import DailyChallengesScreen from './all/containers/screens/DailyChallengesScreen';
+import TopicsScreeen from "./all/containers/screens/TopicsScreen";
+import CustomCourseCategoriesScreen from "./all/containers/screens/CustomCourseCategoriesScreen";
+import MessagesListScreen from "./all/containers/screens/MessagesListScreen";
+import CustomHomeScreen from "./all/containers/screens/CustomHomeScreen";
+import OnboardingScreen from "./all/containers/screens/OnboardingScreen";
 /*----------------------*/
 /*      Components      */
 /*----------------------*/
 // * LD Content * //
-
+import LessonTitle from "./all/components/Screens/LessonSingleScreen/LessonTitle";
+import TopicTitle from "./all/components/Screens/LearnTopicSingleScreen/TopicTitle";
+import PrevNextLessons from './all/components/Screens/LessonSingleScreen/PrevNextButtonsLesson';
+import PrevNextTopics from "./all/components/Screens/LearnTopicSingleScreen/PrevNextButtonsTopic";
+import ContentHeadingsBlock from "./all/components/Global/CoreHeadingBlock";
+import ImageComponent from "./all/components/Global/CoreImageBlock";
+import EmbedsComponent from "./all/components/Global/CoreEmbedBlock";
+import LessonActionComponent from "./all/components/Screens/LessonSingleScreen/LessonBottomActionButton";
+import LearnTopicActionComponent from "./all/components/Screens/LearnTopicSingleScreen/TopicBottomActionButton";
+import SpacerComponent from "./all/components/Global/CoreSpacerBlock";
 // * LD Courses * //
-
+import CourseHeaderItems from "./all/components/Screens/CourseSingleScreen/CourseHeaderDetails";
+import CourseActionButton from "@src/components/Course/CourseActionButton";
 // * Index Screen Defaults * //
-
+import IndexScreenHeaderHeight from "./all/components/Global/AnimatedHeaderHeight";
+import FilterBarComponents from "./all/components/Global/ReplaceFilterBarComponent";
+import CustomTabBarBottom from "./all/components/Global/TabBarBottom";
+import AnimatedHeaderContents from "./all/components/Global/AnimatedHeaderContents";
 // * User Profile * //
-
+import AfterProfileDetails from "./all/components/Screens/ProfileScreen/AfterProfileDetails";
+import ProfileHeaderButton from "./all/components/Screens/ProfileScreen/ProfileHeaderRightButton";
+import UserProfileAvatar from "./all/components/Screens/ProfileScreen/ProfileSubscriberBadge";
 // * Forums * //
-
+import ForumHeaderButtons from './all/components/Screens/ForumsSingleScreen/ForumHeaderRightButtons';
+import HeaderRightComponent from "./all/components/Screens/TopicsScreen/HeaderRight";
+import ReplyItemAvatar from "./all/components/Screens/TopicsSingleScreen/ReplyItemAvatar";
 // * Misc * //
-
+import GroupDetailsComponent from "./all/components/Screens/GroupSingleScreen/GroupDetailsComponent";
+import MessageText from "./all/components/Screens/MessagesScreen/MessageContents";
 // * Main List Items * //
-
+import BlogItem from "./all/components/ListItems/BlogItem";
+import TopicItem from "./all/components/ListItems/TopicItem";
+import TopicHeaderAvatar from "./all/components/Screens/TopicsSingleScreen/TopicHeaderItem";
 /*----------------------*/
 /*       Reducers       */
 /*----------------------*/
