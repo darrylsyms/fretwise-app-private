7d27e99e004f6ccd4a4c5237ae93c63e7c085e74
Darryl <82924984+darrylsyms@users.noreply.github.com>
2023-05-01 15:38:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { NavigationActions } from 'react-navigation';
+import { ScreenNames } from "./all/data/ScreensWithoutTabBar";
 /*----------------------*/
 /*    Custom Screens    */
 /*----------------------*/
@@ -9,12 +10,27 @@ import { NavigationActions } from 'react-navigation';
 /*      Components      */
 /*----------------------*/
 // * LD Content * //
-
+import LessonTitle from "./all/components/Screens/LessonSingleScreen/LessonTitle";
+import TopicTitle from "./all/components/Screens/LearnTopicSingleScreen/TopicTitle";
+import PrevNextLessons from './all/components/Screens/LessonSingleScreen/PrevNextButtonsLesson';
+import PrevNextTopics from "./all/components/Screens/LearnTopicSingleScreen/PrevNextButtonsTopic";
+import ContentHeadingsBlock from "./all/components/Global/CoreHeadingBlock";
+import ImageComponent from "./all/components/Global/CoreImageBlock";
+import EmbedsComponent from "./all/components/Global/CoreEmbedBlock";
+import LessonActionComponent from "./all/components/Screens/LessonSingleScreen/LessonBottomActionButton";
+import LearnTopicActionComponent from "./all/components/Screens/LearnTopicSingleScreen/TopicBottomActionButton";
+import SpacerComponent from "./all/components/Global/CoreSpacerBlock";
 // * LD Courses * //
+import CourseActionButton from "@src/components/Course/CourseActionButton";
 // * Index Screen Defaults * //
+import IndexScreenHeaderHeight from "./all/components/Global/AnimatedHeaderHeight";
+import FilterBarComponents from "./all/components/Global/ReplaceFilterBarComponent";
+import CustomTabBarBottom from "./all/components/Global/TabBarBottom";
+import AnimatedHeaderContents from "./all/components/Global/AnimatedHeaderContents";
 // * User Profile * //
 import AfterProfileDetails from "./all/components/Screens/ProfileScreen/AfterProfileDetails";
-
+import ProfileHeaderButton from "./all/components/Screens/ProfileScreen/ProfileHeaderRightButton";
+import UserProfileAvatar from "./all/components/Screens/ProfileScreen/ProfileSubscriberBadge";
 // * Forums * //
 
 // * Misc * //
