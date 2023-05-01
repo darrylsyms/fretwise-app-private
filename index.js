d5f4effe970166f93ebbd962d14b3863b881c214
Darryl <82924984+darrylsyms@users.noreply.github.com>
2023-05-01 21:45:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,25 +10,11 @@ import { ScreenNames } from "./all/data/ScreensWithoutTabBar";
 /*      Components      */
 /*----------------------*/
 // * LD Content * //
-import LessonTitle from "./all/components/Screens/LessonSingleScreen/LessonTitle";
-import TopicTitle from "./all/components/Screens/LearnTopicSingleScreen/TopicTitle";
-import PrevNextLessons from './all/components/Screens/LessonSingleScreen/PrevNextButtonsLesson';
-import PrevNextTopics from "./all/components/Screens/LearnTopicSingleScreen/PrevNextButtonsTopic";
 import ContentHeadingsBlock from "./all/components/Global/CoreHeadingBlock";
-import LessonActionComponent from "./all/components/Screens/LessonSingleScreen/LessonBottomActionButton";
-import LearnTopicActionComponent from "./all/components/Screens/LearnTopicSingleScreen/TopicBottomActionButton";
-import SpacerComponent from "./all/components/Global/CoreSpacerBlock";
 // * LD Courses * //
 import CourseActionButton from "@src/components/Course/CourseActionButton";
 // * Index Screen Defaults * //
-import IndexScreenHeaderHeight from "./all/components/Global/AnimatedHeaderHeight";
-import FilterBarComponents from "./all/components/Global/ReplaceFilterBarComponent";
-import CustomTabBarBottom from "./all/components/Global/TabBarBottom";
-import AnimatedHeaderContents from "./all/components/Global/AnimatedHeaderContents";
-// * User Profile * //
-import AfterProfileDetails from "./all/components/Screens/ProfileScreen/AfterProfileDetails";
-import ProfileHeaderButton from "./all/components/Screens/ProfileScreen/ProfileHeaderRightButton";
-import UserProfileAvatar from "./all/components/Screens/ProfileScreen/ProfileSubscriberBadge";
+
 // * Forums * //
 
 // * Misc * //
